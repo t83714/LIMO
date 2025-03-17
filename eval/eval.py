accc37b15a37b768d0ecf09842d590293cca8c9f
Jacky Jiang <t83714@gmail.com>
2025-03-17 17:27:09 +1100

    fix RuntimeError: Cannot re-initialize CUDA in forked subprocess issue: https://github.com/vllm-project/vllm/issues/6152

--- eval/eval.py
+++ eval/eval.py
@@ -18,7 +18,6 @@ from utils.math_normalization import *
 from utils.grader import *
 import pickle
 from math import comb
-import torch.multiprocessing as mp
 
 # envs.VLLM_HOST_IP="0.0.0.0" or "127.0.0.1"
 
@@ -244,7 +243,7 @@ def infer(args):
 
 
 if __name__ == "__main__":
-    mp.set_start_method('spawn', force=True)
+    os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
     args = parse_args()
     set_seed(args.seed)
     infer(args)
