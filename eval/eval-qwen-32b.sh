d0dcdae1b262ced75f63809b564c1496cad21090
Jacky Jiang <t83714@gmail.com>
2025-03-26 00:42:56 +1100

    add qwen-32b lora eval script

---
+++ eval/eval-qwen-32b.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICES='0,1' \
+python eval.py \
+--model_name_or_path "Qwen/Qwen2.5-32B-Instruct" \
+--lora-adapter_name_or_path "t83714/qwen2.5-32b-instruct-limo-lora-adapter" \
+--enable-lora True \
+--data_name "math" \
+--prompt_type "qwen-instruct" \
+--temperature 0.0 \
+--start_idx 0 \
+--end_idx -1 \
+--n_sampling 1 \
+--k 1 \
+--split "test" \
+--max_tokens 32768 \
+--seed 0 \
+--top_p 1 \
+--surround_with_messages \
+
